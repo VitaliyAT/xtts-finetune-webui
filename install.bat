9ef533a6c1ec7108d813b05744fd87e210c77ee0
VitTim <t@promteko.ru>
2024-05-26 00:57:37 +0500

    normal install

--- install.bat
+++ install.bat
@@ -1,7 +1,10 @@
 @echo off
 
-python -m venv venv
+py -3.10 -m venv venv
+
 call venv/scripts/activate
+
+py -m pip install --upgrade pip
 
 pip install torch==2.1.1+cu118 torchaudio==2.1.1+cu118 --index-url https://download.pytorch.org/whl/cu118
 pip install -r .\requirements.txt
